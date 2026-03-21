afce634d29b0adb7b69ab3e73f4d997784d779eb
ritu <ritukumari1967sharma@gmail.com>
2026-03-22 00:49:28 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const filepath = path.join(__dirname, "index.html");
+
+app.get('/' , (req,res) =>{
+    res.sendFile(filepath);
+})
+
+
+app.listen(8000,() => {
+    console.log(`listen at http://localhost:${8000}`);
+})
